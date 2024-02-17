43e75b83fb3c10e49e33226e419f00db0152c9a7
Luis Gil <giluis2000@gmail.com>
2024-02-17 14:34:18 -0400

    quitando pruebas en concursos

--- src/fundaStohr/pages/concursos/Concursos.jsx
+++ src/fundaStohr/pages/concursos/Concursos.jsx
@@ -3,32 +3,5 @@ import { Button, List, ListItem, Typography } from "@mui/material";
 import React from "react";
 
 export const Concursos = () => {
-  return (
-    <div>
-      <Typography
-        variant="h1"
-        sx={{ color: "red", ":hover": { color: "green" } }}
-      >
-        Concursos
-      </Typography>
-
-      <List>
-        <ListItem>
-          <Button
-            startIcon={<LunchDiningOutlined />}
-            endIcon={<MyLocationOutlined />}
-          >
-            Hola
-          </Button>
-        </ListItem>
-      </List>
-
-      <Typography
-        variant="h2"
-        sx={{ color: "blue", ":hover": { color: "red" } }}
-      >
-        Concursos
-      </Typography>
-    </div>
-  );
+  return <div>Concursos</div>;
 };
